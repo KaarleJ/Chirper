4b79398efb5a27f0020f50fffbacd672d784c297
Kaarle Järvinen <kaarle46@gmail.com>
2024-10-15 09:37:04 +0300

    Chirptype

--- resources/js/types/index.d.ts
+++ resources/js/types/index.d.ts
@@ -5,6 +5,14 @@ export interface User {
     email_verified_at?: string;
 }
 
+export interface Chirp {
+    id: number;
+    user: User;
+    message: string;
+    created_at: string;
+    updated_at: string;
+}
+
 export type PageProps<
     T extends Record<string, unknown> = Record<string, unknown>,
 > = T & {
