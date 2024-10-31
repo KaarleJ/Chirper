35da0f4f3565dc15b89ea6d4f37b290373bf9ef5
Kaarle Järvinen <kaarle46@gmail.com>
2024-10-31 10:54:33 +0200

    Created customized link

--- resources/js/Components/NavLink.tsx
+++ resources/js/Components/NavLink.tsx
@@ -1,23 +1,22 @@
-import { InertiaLinkProps, Link } from '@inertiajs/react';
+import { cn } from "@/lib/utils";
+import { Link, InertiaLinkProps } from "@inertiajs/react";
 
 export default function NavLink({
-    active = false,
-    className = '',
-    children,
-    ...props
-}: InertiaLinkProps & { active: boolean }) {
-    return (
-        <Link
-            {...props}
-            className={
-                'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
-                (active
-                    ? 'border-indigo-400 text-gray-900 focus:border-indigo-700'
-                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700') +
-                className
-            }
-        >
-            {children}
-        </Link>
-    );
+  href,
+  children,
+  className,
+  ...props
+}: InertiaLinkProps) {
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "flex items-center gap-4 text-foreground text-xl font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
 }
