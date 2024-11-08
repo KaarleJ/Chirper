fa5825782311b05093bd398fb51cbf952db55300
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-08 10:38:29 +0200

    Refactored to use shadcn passwords

--- resources/js/Pages/Profile/Partials/UpdatePasswordForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdatePasswordForm.tsx
@@ -42,14 +42,6 @@ export default function UpdatePasswordForm({
 
   return (
     <section className={className}>
-      <header>
-        <h2 className="text-lg font-medium text-gray-900">Update Password</h2>
-
-        <p className="mt-1 text-sm text-gray-600">
-          Ensure your account is using a long, random password to stay secure.
-        </p>
-      </header>
-
       <form onSubmit={updatePassword} className="mt-6 space-y-6">
         <div>
           <Label htmlFor="current_password">Current password</Label>
--- resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
+++ resources/js/Pages/Profile/Partials/UpdateProfileInformationForm.tsx
@@ -30,16 +30,6 @@ export default function UpdateProfileInformation({
 
   return (
     <section className={className}>
-      <header>
-        <h2 className="text-lg font-medium text-gray-900">
-          Profile Information
-        </h2>
-
-        <p className="mt-1 text-sm text-gray-600">
-          Update your account's profile information and email address.
-        </p>
-      </header>
-
       <form onSubmit={submit} className="mt-6 space-y-6">
         <div>
           <Label htmlFor="name">Name</Label>
