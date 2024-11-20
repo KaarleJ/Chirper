af70bcfb7309beab7daddb154ffc17054b9db8ed
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-20 09:49:31 +0200

    + style

--- resources/js/Pages/Chats/Index.tsx
+++ resources/js/Pages/Chats/Index.tsx
@@ -1,16 +1,29 @@
 import Header from "@/Components/Header";
+import { Button } from "@/Components/ui/button";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Chat } from "@/types";
 import { Head } from "@inertiajs/react";
+import { MailPlus as NewMessage } from "lucide-react";
 
-export default function Chats() {
+export default function Chats(/*{ chats }: { chats: Chat }*/) {
+  const chats: Chat[] = [
+    {
+      userOne: 1,
+      userTwo: 2,
+      messages: [],
+    },
+  ];
   return (
     <AuthenticatedLayout>
       <Head title="Chats" />
-      <Header title="Chats" />
-
-      <div className="py-12">
-        Chats
+      <div className="flex items-center justify-between border-b">
+        <Header title="Chats" className="border-b-0" />
+        <Button className="mr-12 rounded-full p-2" size="icon">
+          <NewMessage />
+        </Button>
       </div>
+
+      <div className="py-12">Chats</div>
     </AuthenticatedLayout>
   );
 }
