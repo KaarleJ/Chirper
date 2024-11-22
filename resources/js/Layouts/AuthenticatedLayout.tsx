1b789edd094cabd01fd8f509bcfa1fbda47eeea4
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-22 18:09:36 +0200

    Ability to hide searchbar

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -1,17 +1,19 @@
 import Header from "@/Components/Header";
 import SearchBar from "@/Components/SearchBar";
 import SideNav from "@/Components/SideNav";
+import { cn } from "@/lib/utils";
 import { PropsWithChildren } from "react";
 
-export default function Authenticated({ children }: PropsWithChildren) {
-
+export default function Authenticated({
+  children,
+  className,
+  hideSearch,
+}: PropsWithChildren & { hideSearch?: boolean; className?: string }) {
   return (
     <div className="min-h-screen flex justify-between item-start px-8 md:px-18 2xl:px-52">
       <SideNav />
-      <main className="min-h-screen w-full">
-        {children}
-      </main>
-      <SearchBar />
+      <main className={cn("min-h-screen w-full", className)}>{children}</main>
+      {!hideSearch && <SearchBar />}
     </div>
   );
 }
