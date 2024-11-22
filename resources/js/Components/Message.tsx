6333a50909a3aad15e11e587f615e95917099cd2
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-22 18:14:42 +0200

    Added timestamps to message component

--- resources/js/Components/Message.tsx
+++ resources/js/Components/Message.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { User, Message as MessageType } from "@/types";
+import { format } from "date-fns";
 
 export default function Message({
   message,
@@ -14,11 +15,14 @@ export default function Message({
   return (
     <div
       className={cn(
-        "border rounded-lg p-2 shadow-md w-1/2",
+        "border rounded-lg px-4 py-2 shadow-md w-1/2",
         isSender && "self-end"
       )}
     >
       <p>{message.content}</p>
+      <p className="text-right text-sm text-gray-400">
+        {format(message.created_at, "HH.mm")}
+      </p>
     </div>
   );
 }
