dc0f6f2aee7fbc51cbc41f29f86314845cdc9342
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-22 18:09:27 +0200

    Created the chatscreen for chats

--- resources/js/Pages/Chats/Index.tsx
+++ resources/js/Pages/Chats/Index.tsx
@@ -4,11 +4,15 @@ import { Chat, Message, PageProps, User } from "@/types";
 import { Head } from "@inertiajs/react";
 import CreateChatDialog from "@/Components/CreateChatDialog";
 import ChatCard from "@/Components/ChatCard";
+import ChatScreen from "@/Components/ChatScreen";
+import { Button } from "@/Components/ui/button";
+import { MailPlus as NewMessage } from "lucide-react";
 
 export default function Chats({
   follows,
   chats,
   auth,
+  messages,
   currentChat,
 }: PageProps & {
   follows: User[];
@@ -16,24 +20,35 @@ export default function Chats({
   messages: Message[];
   currentChat?: Chat;
 }) {
-  console.log(chats);
   return (
-    <AuthenticatedLayout>
+    <AuthenticatedLayout hideSearch className="border-r">
       <Head title="Chats" />
-      <div className="flex items-center justify-between border-b">
-        <Header title="Chats" className="border-b-0" />
-        <CreateChatDialog follows={follows} />
-      </div>
+      <div className="flex flex-row h-full">
+        <div className="flex flex-col w-1/3 border-r">
+          <div className="flex items-center justify-between border-b h-[6rem]">
+            <Header title="Chats" className="border-b-0 w-min" />
+            <CreateChatDialog follows={follows}>
+              <Button className="mr-12 rounded-full p-2" size="icon">
+                <NewMessage />
+              </Button>
+            </CreateChatDialog>
+          </div>
 
-      <div>
-        {chats.map((chat) => (
-          <ChatCard
-            key={chat.id}
-            chat={chat}
-            auth={auth}
-            selected={currentChat?.id === chat.id}
-          />
-        ))}
+          <div className="h-full">
+            {chats.map((chat) => (
+              <ChatCard
+                key={chat.id}
+                chat={chat}
+                auth={auth}
+                selected={currentChat?.id === chat.id}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-2/3">
+          <ChatScreen chat={currentChat} auth={auth} messages={messages} />
+        </div>
       </div>
     </AuthenticatedLayout>
   );
