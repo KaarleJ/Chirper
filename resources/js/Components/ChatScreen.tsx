bd273409568912d924fe4aa1afdf4db82cd55f1a
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-27 15:35:17 +0200

    Messages are grouped under different dates

--- resources/js/Components/ChatScreen.tsx
+++ resources/js/Components/ChatScreen.tsx
@@ -2,7 +2,7 @@ import { Chat, Message as MessageType, User } from "@/types";
 import CreateChatDialog from "./CreateChatDialog";
 import { useForm, usePage } from "@inertiajs/react";
 import { Button } from "./ui/button";
-import { resolveChatPartner } from "@/lib/utils";
+import { getFormattedDate, resolveChatPartner } from "@/lib/utils";
 import { UserCard } from "./UserCard";
 import Message from "./Message";
 import { FormEvent } from "react";
@@ -40,6 +40,15 @@ export default function ChatScreen({
     });
   }
 
+  const groupedMessages = messages?.reduce((acc, message) => {
+    const messageDate = getFormattedDate(message.created_at);
+    if (!acc[messageDate]) {
+      acc[messageDate] = [];
+    }
+    acc[messageDate].push(message);
+    return acc;
+  }, {} as Record<string, MessageType[]>);
+
   return (
     <div className="max-h-screen h-screen flex flex-col justify-start">
       <div className="h-[5.8rem] border-b flex items-center px-4">
@@ -47,19 +56,25 @@ export default function ChatScreen({
       </div>
       <div className="p-4 flex flex-col justify-between items-stretch">
         <div className="flex flex-col-reverse h-[44rem] 4xl:h-[65rem] overflow-y-auto">
-          {messages?.map((message) => (
-            <Message
-              key={message.chat_id + message.sender_id + message.created_at}
-              message={message}
-              auth={auth}
-              chatPartner={chatPartner}
-            />
-          ))}
+          {groupedMessages &&
+            Object.entries(groupedMessages).map(([date, messages]) => (
+              <div key={date} className="flex flex-col-reverse">
+                {messages.map((message) => (
+                  <Message
+                    key={
+                      message.chat_id + message.sender_id + message.created_at
+                    }
+                    message={message}
+                    auth={auth}
+                  />
+                ))}
+                <div className="text-center text-gray-500 text-sm my-2">
+                  {date}
+                </div>
+              </div>
+            ))}
         </div>
-        <form
-          onSubmit={submit}
-          className="p-4 flex gap-2"
-        >
+        <form onSubmit={submit} className="p-4 flex gap-2">
           <Input
             type="text"
             value={data.content}
