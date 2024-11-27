503378573c6058e900cdb7f13cffc0bfe197de20
Kaarle Järvinen <kaarle46@gmail.com>
2024-11-27 15:35:02 +0200

    Util function for formatting dates

--- resources/js/lib/utils.ts
+++ resources/js/lib/utils.ts
@@ -1,5 +1,6 @@
 import { Chat, User } from "@/types";
 import { clsx, type ClassValue } from "clsx";
+import { format, isToday, isYesterday, parseISO } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +11,11 @@ export function resolveChatPartner(auth: { user: User }, chat: Chat) {
   const user =
     chat.user_one.id === auth.user.id ? chat.user_two : chat.user_one;
   return user;
+}
+
+export function getFormattedDate(dateString: string) {
+  const date = parseISO(dateString);
+  if (isToday(date)) return "Today";
+  if (isYesterday(date)) return "Yesterday";
+  return format(date, "MMMM dd, yyyy");
 }
