4d90b98fa39e076ad62f1e5158db1ee09f9ad23f
Kaarle Järvinen <kaarle46@gmail.com>
2024-12-11 14:17:53 +0200

    ...

--- database/migrations/2024_12_11_103851_create_comments_table.php
+++ database/migrations/2024_12_11_103851_create_comments_table.php
@@ -12,9 +12,9 @@ return new class extends Migration {
   {
     Schema::create('comments', function (Blueprint $table) {
       $table->id();
-      $table->foreignId('chirp_id')->constrained()->onDelete('cascade'); // Link to chirps
-      $table->foreignId('user_id')->constrained()->onDelete('cascade'); // User who created the comment
-      $table->text('content'); // Comment content
+      $table->foreignId('chirp_id')->constrained()->onDelete('cascade');
+      $table->foreignId('user_id')->constrained()->onDelete('cascade');
+      $table->text('content');
       $table->timestamps();
     });
   }
