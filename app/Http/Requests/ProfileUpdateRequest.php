55bd343d2360414663d6ad63ea426af39985c0c1
Kaarle Järvinen <kaarle46@gmail.com>
2024-12-18 11:36:35 +0200

    Unified format

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use App\Models\User;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
@@ -16,15 +14,8 @@ class ProfileUpdateRequest extends FormRequest
   public function rules(): array
   {
     return [
-      'username' => ['required', 'string', 'max:255'],
-      'email' => [
-        'required',
-        'string',
-        'lowercase',
-        'email',
-        'max:255',
-        Rule::unique(User::class)->ignore($this->user()->id),
-      ],
+      'username' => 'required|string|max:255',
+      'email' => 'required|string|email|max:255',
     ];
   }
 }
